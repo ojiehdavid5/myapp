0eda263919688f6b5b7400a31d3bdef0cd92168a
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-07-30 19:27:49 +0100

    done

---
+++ components/Todoitem.tsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View ,TouchableOpacity} from 'react-native'
+
+export default function Todoitem({item,pressHandler })  {
+   {
+    return (
+        <TouchableOpacity onPress={()=>pressHandler(item.key)}>
+        <Text style={styles.item}> {item.text}
+          
+          
+           </Text>
+ 
+        </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+   item:{
+      padding:16,
+      marginTop:16,
+      borderColor:'#bbb',
+      borderStyle:'dashed',
+borderRadius:10,    
+fontSize:20  
+   }
+  
+})
