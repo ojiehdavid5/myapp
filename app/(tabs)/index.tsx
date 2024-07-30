30f4121671969b40a6b977c36ee03c4e44fcac5c
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-07-30 19:25:39 +0100

    almost done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,95 +5,78 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Header from './Header'
+import Todoitem from '../../components/Todoitem'
+import Addtodo from '../../components/addTodo'
 
 export default function HomeScreen() {
+const [todos,setTodos]=useState([
+  {text:"Buy coffe",key:'1'},
+  {text:"Buy coffe",key:'2'},
+  {text:"Buy coffe",key:'3'},
+  {text:"Buy coffe",key:'4'},
+  {text:"Buy coffe",key:'5'}
+])
 
-  // const [name,setName]=useState('chuks');
-  // const [age,setAge]=useState(40);
+const pressHandler=(key)=>{
+  setTodos((prevTodos)=>{
+    return prevTodos.filter(todos=>todos.key!= key)
+  })
+}
 
-  const [people,setPeople]=useState([
-    {name:"chuks",age:10},
-    {name:"emma",age:10},
-    {name:"sam",age:10},
-    {name:"tobi",age:10},
-    {name:"divine",age:10},
-    {name:"kelvin",age:124},
-    {name:"gideon",age:10},
-    {name:"favour",age:11},
-    {name:"edet",age:10},
-  ])
+
+const submitHandler=(text)=>{
+  setTodos((prevTodos)=>{
+    return[
+      {text:text,key:Math.random().toString()},
+      ...prevTodos
+    ]
+  })
+}
+
   return (
-    <View style={styles.background} >
-      <FlatList
-      keyExtractor={(item)=>item.age}
-      data={people}
+    <View style={styles.container} >
+      <Header/>
+      <View style={styles.content }>
+        <Addtodo submitHandler={submitHandler}/>
+        <View style={styles.list}>
+        <FlatList
+      data={todos}
       renderItem={({item})=>(
-        <Text style={styles.item}>{item.name}</Text>
+        <Todoitem item={item} pressHandler={pressHandler}/>
+        
+
       )}
       
       
       
       />
 
-
-
-
-      {/* {people.map((item)=>(
-        <View>
-          <Text>
-            {item.name}
-          </Text>
         </View>
-      ))} */}
+      </View>
 
 
 
-
-
-
-      {/* <Text>Enter Name:</Text>
-      <TextInput style={styles.input} 
-      placeholder='e.g ojieh david chukwuyenum'
-
-      onChangeText={(val)=>setName(val)}
-      
-      />
-      <Text>Enter :</Text>
-      <TextInput style={styles.input} 
-      placeholder='e.g 20'
-      onChangeText={(val)=>setAge(val)}
-      
-      />
-
-     <Text> name:{name},age:{age}</Text> */}
      
     </View>
+    
   )
 
 }
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor:"white",
+  container:{
     flex:1,
-
-
-    paddingTop:40,
-    paddingHorizontal:20,
-
+    backgroundColor:'#fff',
 
   },
-  input:{
-    borderWidth:1,
-    borderColor:'#777',
-    padding:8,
-    margin:20,
-    width:200
+
+  content:{
+    padding:4
   },
-  item:{
-    marginTop:24,
-    padding:30,
-    backgroundColor:'pink',
-    fontSize:24,
+  list:{
+    marginTop:20
   }
+
+
 });
